8f37e69b229b6237f1863e5471f6445177baded8
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2023-02-14 15:57:21 -0400

    fix tests based on clarification of inflation question

--- test_wrangling_data.py
+++ test_wrangling_data.py
@@ -21,13 +21,13 @@ class TestDataWrangling:
         inflation_index = df.get("inflation_index")
         assert inflation_index is not None
         assert inflation_index.max() == pytest.approx(1.327808, abs=0.001)
-        assert df.query("year == 2017").inflation_index.iloc[0] == pytest.approx(1.08243, abs=0.001)
+        assert df.query("year == 2017").inflation_index.iloc[0] == pytest.approx(1.251222, abs=0.001)
 
     def test_dw_4(self):
         inflated_loss_ratio = df.get("inflated_loss_ratio")
         assert inflated_loss_ratio is not None
-        assert inflated_loss_ratio.max() == pytest.approx(0.617374, abs=0.001)
-        assert df.query("year == 2017").inflated_loss_ratio.iloc[0] == pytest.approx(0.462771, abs=0.001)
+        assert inflated_loss_ratio.max() == pytest.approx(0.554702, abs=0.001)
+        assert df.query("year == 2017").inflated_loss_ratio.iloc[0] == pytest.approx(0.534934, abs=0.001)
 
     def test_dw_5(self):
         assert wrangling_data.all_year_loss_ratio(df) == pytest.approx(0.441244, abs=0.001)
