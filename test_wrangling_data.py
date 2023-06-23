dd86351b7648a0b452f76b77cb1838e3abfbb020
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2023-06-23 17:07:17 -0300

    change sorted test to use pandas.Series.is_monotonic_increasing

--- test_wrangling_data.py
+++ test_wrangling_data.py
@@ -5,6 +5,7 @@ import pytest
 
 df = wrangling_data.df
 
+
 # TESTS
 class TestDataWrangling01:
     def test_df_is_df(self):
@@ -16,7 +17,8 @@ class TestDataWrangling01:
         assert expected_cols.issubset(observed_cols)
 
     def test_df_is_sorted(self):
-        assert sorted(df.year.to_list()) == df.year.to_list()
+        assert df.year.is_monotonic_increasing
+
 
 class TestDataWrangling02:
     def test_columns_present(self):
@@ -36,11 +38,12 @@ class TestDataWrangling02:
                 0.444,
                 0.469,
                 0.465,
-            ]
-        )
+            ],
+        ),
     )
     def test_actual_loss_ratio(self, year, expected):
         assert check_year_value(df, year, "actual_loss_ratio", expected)
+
 
 class TestDataWrangling03:
     def test_columns_present(self):
@@ -60,11 +63,12 @@ class TestDataWrangling03:
                 1.168,
                 1.134,
                 1.090,
-            ]
-        )
+            ],
+        ),
     )
     def test_inflation_index(self, year, expected):
         assert check_year_value(df, year, "inflation_index", expected)
+
 
 class TestDataWrangling04:
     def test_columns_present(self):
@@ -84,34 +88,27 @@ class TestDataWrangling04:
                 0.518,
                 0.532,
                 0.507,
-
-            ]
-        )
+            ],
+        ),
     )
     def test_inflated_loss_ratio(self, year, expected):
         check_year_value(df, year, "inflated_loss_ratio", expected)
 
+
 class TestDataWrangling05:
     def all_years_lr_correct(self):
         assert wrangling_data.all_year_loss_ratio(df) == pytest.approx(0.441, abs=0.001)
+
 
 class TestDataWrangling06:
     @pytest.mark.parametrize(
         "n,expected",
         zip(
             range(9, 0, -1),
-            [
-                0.441,
-                0.445,
-                0.449,
-                0.453,
-                0.458,
-                0.459,
-                0.460,
-                0.467,
-                0.465
-            ]
-        )
+            [0.441, 0.445, 0.449, 0.453, 0.458, 0.459, 0.460, 0.467, 0.465],
+        ),
     )
     def last_n_years_lr_correct(self, n, expected):
-        assert wrangling_data.last_n_year_loss_ratio(df, n) == pytest.approx(expected, abs=0.001)
+        assert wrangling_data.last_n_year_loss_ratio(df, n) == pytest.approx(
+            expected, abs=0.001
+        )
